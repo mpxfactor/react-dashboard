9e4b4efde515fb9416676496c6bf5ef8a16ca784
mpxfactor <tancmpsinc@gmail.com>
2022-12-08 13:10:53 +0530

    Working

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -15,82 +15,84 @@ import LocalShippingIcon from "@mui/icons-material/LocalShipping";
 import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-  return (
-    <div className="sidebar">
-      <div className="top">
-        <Link to="/" style={{ textDecoration: "none" }}>
-          <span className="logo">CreSo</span>
-        </Link>
-      </div>
-      <hr />
-      <div className="center">
-        <ul>
-          <p className="title">MAIN</p>
-          <li>
-            <DashboardIcon className="icon" />
-            <span>Dashboard</span>
-          </li>
-          <p className="title">LISTS</p>
-          <Link to="/users" style={{ textDecoration: "none" }}>
-            <li>
-              <PeopleAltIcon className="icon" />
-              <span>Users</span>
-            </li>
-          </Link>
-          <Link to="/products" style={{ textDecoration: "none" }}>
-            <li>
-              <Inventory2Icon className="icon" />
-              <span>Products</span>
-            </li>
-          </Link>
+    return (
+        <div className="sidebar">
+            <div className="top">
+                <Link to="/" style={{ textDecoration: "none" }}>
+                    <span className="logo">CreSo</span>
+                </Link>
+            </div>
+            <hr />
+            <div className="center">
+                <ul>
+                    <p className="title">MAIN</p>
+                    <Link to="/" style={{ textDecoration: "none" }}>
+                        <li>
+                            <DashboardIcon className="icon" />
+                            <span>Dashboard</span>
+                        </li>
+                    </Link>
+                    <p className="title">LISTS</p>
+                    <Link to="/users" style={{ textDecoration: "none" }}>
+                        <li>
+                            <PeopleAltIcon className="icon" />
+                            <span>Users</span>
+                        </li>
+                    </Link>
+                    <Link to="/products" style={{ textDecoration: "none" }}>
+                        <li>
+                            <Inventory2Icon className="icon" />
+                            <span>Products</span>
+                        </li>
+                    </Link>
 
-          <li>
-            <ViewStreamIcon className="icon" />
-            <span>Orders</span>
-          </li>
-          <li>
-            <LocalShippingIcon className="icon" />
-            <span>Delivery</span>
-          </li>
-          <p className="title">USEFUL</p>
-          <li>
-            <NotificationsIcon className="icon" />
-            <span>Notification</span>
-          </li>
-          <li>
-            <PollIcon className="icon" />
-            <span>Stats</span>
-          </li>
-          <p className="title">SERVICE</p>
-          <li>
-            <HealthAndSafetyIcon className="icon" />
-            <span>System Health</span>
-          </li>
-          <li>
-            <InputIcon className="icon" />
-            <span>Logs</span>
-          </li>
-          <li>
-            <SettingsIcon className="icon" />
-            <span>Settings</span>
-          </li>
-          <p className="title">USER</p>
-          <li>
-            <AccountBoxIcon className="icon" />
-            <span>Profile</span>
-          </li>
-          <li>
-            <LogoutIcon className="icon" />
-            <span>Logout</span>
-          </li>
-        </ul>
-      </div>
-      <div className="bottom">
-        <div className="colorOption"></div>
-        <div className="colorOption"></div>
-      </div>
-    </div>
-  );
+                    <li>
+                        <ViewStreamIcon className="icon" />
+                        <span>Orders</span>
+                    </li>
+                    <li>
+                        <LocalShippingIcon className="icon" />
+                        <span>Delivery</span>
+                    </li>
+                    <p className="title">USEFUL</p>
+                    <li>
+                        <NotificationsIcon className="icon" />
+                        <span>Notification</span>
+                    </li>
+                    <li>
+                        <PollIcon className="icon" />
+                        <span>Stats</span>
+                    </li>
+                    <p className="title">SERVICE</p>
+                    <li>
+                        <HealthAndSafetyIcon className="icon" />
+                        <span>System Health</span>
+                    </li>
+                    <li>
+                        <InputIcon className="icon" />
+                        <span>Logs</span>
+                    </li>
+                    <li>
+                        <SettingsIcon className="icon" />
+                        <span>Settings</span>
+                    </li>
+                    <p className="title">USER</p>
+                    <li>
+                        <AccountBoxIcon className="icon" />
+                        <span>Profile</span>
+                    </li>
+                    <li>
+                        <LogoutIcon className="icon" />
+                        <span>Logout</span>
+                    </li>
+                </ul>
+            </div>
+            <div className="bottom">
+                <div className="colorOption"></div>
+                <div className="colorOption"></div>
+            </div>
+        </div>
+    );
 };
 
 export default Sidebar;
